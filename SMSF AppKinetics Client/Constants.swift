e1c1eacc5e696ca05f09b7c832f153e06933feb6
David Fekke <david@fekke.com>
2022-01-07 12:43:54 -0500

    Adding first commit

---
+++ SMSF AppKinetics Client/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Dynamics SwiftUI Sample
+//
+//  Created by David Fekke on 1/7/22.
+//
+
+import Foundation
+
+/*
+ Import Contact (com.swyftmobile.smsf.create-contact)
+  o This service allows BlackBerry Dynamics apps to import a contact in VCF format into Salesforce as either a Contact record or a Lead record with Swyft Mobile for Salesforce app.
+  • Import Document (com.swyftmobile.smsf.create-document)
+  o This service allows BlackBerry Dynamics apps to import documents or Files into Salesforce and the Salesforce file system with Swyft Mobile for Salesforce app.
+
+  • Import Note (com.swyftmobile.smsf.create-note)
+  o This service allows BlackBerry Dynamics apps to import notes into Salesforce as Note records using Swyft Mobile for Salesforce.
+  • Save Edited File Service (com.good.gdservice.save-edited-file)
+  o This service is for returning a file from an editor application. The file would have originated in the application that is the provider of the service, and would have been sent to the editor application by means of the Edit File Service
+ 
+ */
+
+struct constants {
+    let kImportContact          = "com.swyftmobile.smsf.create-contact"
+    let kImportDocument         = "com.swyftmobile.smsf.create-document"
+    let kImportNote             = "com.swyftmobile.smsf.create-note"
+    let kSaveEditedFileService  = "com.good.gdservice.save-edited-file"
+}
